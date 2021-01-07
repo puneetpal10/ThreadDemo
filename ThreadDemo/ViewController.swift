07c1cb535b93cff323d4255af3d14d18ed8168c1
Puneet Pal <puneet09876@gmail.com>
2021-01-07 21:30:17 +0530

    initial commit

--- ThreadDemo/ViewController.swift
+++ ThreadDemo/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
-
+/*
+ 
+ Thread           Task
+ Serial     +     Sync     Order
+ Serial     +     Async    Order
+ Concurrent +     Sync     Order
+ Concurrent +     Async    UnOrder
+ **/
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.queueTesting()
     }
 
+    func queueTesting(){
+        let myQueue = DispatchQueue(label: "puneet.serial.queue", attributes: .concurrent)
+        
+        //First thread
+        
+        myQueue.async {
+            print("Task 1 started")
+            for index in 1...5{
+                print("\(index) [Task 1] time 5 is \(index * 5)")
+            }
+            print("Task 1 Finished")
+
+        }
+        
+        
+        //Second thread
+        
+        myQueue.async {
+            print("Task 2 started")
+//            for index in 1...5{
+//                print("\(index) [Task 1] time 5 is \(index * 5)")
+//                print("Task 1 Finished")
+//            }
+            print("Task 2 Finished")
+
+        }
+    }
 
 }
 
