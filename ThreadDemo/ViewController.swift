c1eb28d7c96bbfc2f43fb2e3e610f5b1f07bd4b9
Puneet Pal <puneet09876@gmail.com>
2021-01-07 21:14:21 +0530

    Initial Commit

---
+++ ThreadDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadDemo
+//
+//  Created by PuNeet on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
